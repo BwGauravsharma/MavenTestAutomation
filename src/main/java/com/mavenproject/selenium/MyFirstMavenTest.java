fc061a09f021fd7d0573742d408034b14b2740e3
unknown <gauravs@U2-DPE-CPB-0058.bitwiseglobal.net>
2015-11-03 14:45:26 +0530

    Initial Commit

---
+++ src/main/java/com/mavenproject/selenium/MyFirstMavenTest.java
@@ -0,0 +1,26 @@
+package com.mavenproject.selenium;
+
+import static org.junit.Assert.assertEquals;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+public class MyFirstMavenTest {
+	
+	@Test
+	public void StartWebDriver(){
+		
+		WebDriver driver = new FirefoxDriver();
+		driver.get("http://www.bitwiseglobal.com");
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		
+		assertEquals("Bitwise", driver.getTitle());
+		
+		driver.close();
+	}
+
+}
